07ee7b7b16e53b77464ccf2fb627f9ad73763c23
MrLee1998 <2713804696@qq.com>
2020-10-26 09:29:42 +0800

    算法

---
+++ algorithm/LeetCode/2.js
@@ -0,0 +1,17 @@
+var smallerNumbersThanCurrent = function(nums) {
+    var nums = [8, 1, 2, 2, 3];
+    var m;
+    var arr = new Array;
+
+    for (var i = 0; i < nums.length; i++) {
+        m = 0
+        for (var j = 0; j < nums.length; j++) {
+            if (nums[i] > nums[j]) {
+                m++
+            }
+        }
+        arr.push(m)
+    }
+    console.log(arr);
+};
+smallerNumbersThanCurrent()
