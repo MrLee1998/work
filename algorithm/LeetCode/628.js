f3b75189559c9a7db85f24e938ade18ed5b64743
MrLee1998 <2713804696@qq.com>
2020-11-29 00:11:39 +0800

    new

---
+++ algorithm/LeetCode/628.js
@@ -0,0 +1,11 @@
+var maximumProduct = function(nums) {
+  let len = nums.length
+  nums.sort(  (a, b)  => b - a)
+  let result1 = nums[len - 1] * nums[len - 2] * nums[0]
+  let result2 = nums[0] * nums[1] * nums[2]
+  let result3 = nums[0] * nums[1] * nums[len - 1]
+  return Math.max(result1, result2, result3)
+};
+let nums = [1, 2, 3 , 4]
+console.log(maximumProduct(nums));
+console.log(nums);
