4ac268d2d3e41071499721715cd001b0538ece38
MrLee1998 <2713804696@qq.com>
2020-12-01 00:48:30 +0800

    new

---
+++ algorithm/LeetCode/228.js
@@ -0,0 +1,21 @@
+var summaryRanges = function (nums) {
+  let start = 0
+  let end = 0
+  let ans = []
+  let len = nums.length-1
+  for(let i=0;i<len+1;i++){
+
+    end = i
+    if(nums[i]+1!=nums[i+1]){
+      if(start===end){
+        ans.push(nums[end]+'')
+      }else{
+        ans.push(nums[start]+'->'+nums[end])
+      }
+      start = end = i+1
+    }
+  }
+  return ans
+};
+let nums = [0,1,2,4,5,7]
+console.log(summaryRanges(nums));
