30d070d25b249a1d0576b6c2df0faf83c713663f
MrLee1998 <2713804696@qq.com>
2020-12-18 16:40:01 +0800

    new

---
+++ algorithm/基础知识/array.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3]
+let node1 ={val:1, next: null}
+let node2 ={val:2, next: null}
+let node3 ={val:3, next: null}
+node1.next = node2;
+node2.next = node3;
+// console.log(node1);
+let current = node1;
+while (current) {
+    console.log(current);
+    current = current.next;
+}
+
+// 数组 一段连续的空间 
+// 链表 非连续的
