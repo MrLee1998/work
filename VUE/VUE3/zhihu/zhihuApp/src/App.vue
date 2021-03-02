c9c5232137f2ec3ac464f62a2394472635518775
MrLee1998 <2713804696@qq.com>
2021-03-02 22:58:41 +0800

    vue

---
+++ VUE/VUE3/zhihu/zhihuApp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+    哈哈哈
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+.index {
+  color: red;
+  position: fixed;
+  left: 0;
+}
+</style>
